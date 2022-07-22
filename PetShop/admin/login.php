7a2a7f28179a29dd4656df65162c43cb0b30c5eb
thuanhtong <xxxthuanhxxx@gmail.com>
2022-07-22 18:59:15 +0700

    update utils for admin login

--- PetShop/admin/login.php
+++ PetShop/admin/login.php
@@ -50,11 +50,11 @@
         </div>
 
         <!-- jQuery -->
-        <script src="plugins/jquery/jquery.min.js"></script>
+        <script src="utils/plugins/jquery/jquery.min.js"></script>
         <!-- Bootstrap 4 -->
-        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="utils/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
         <!-- AdminLTE App -->
-        <script src="dist/js/adminlte.min.js"></script>
+        <script src="utils/dist/js/adminlte.min.js"></script>
 
         <script>
         $(document).ready(function(){
