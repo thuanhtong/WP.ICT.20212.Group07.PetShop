839345b7e6ae8e4c083ab3ea8ab1ff71c879d113
giangntt <nguyenthugiang106@gmail.com>
2022-07-22 17:59:08 +0700

    refactor

--- PetShop/include/header.php
+++ PetShop/include/header.php
@@ -8,41 +8,41 @@
 
     <link rel="icon" href="<?php echo validate_image($_settings->info('logo')) ?>" />
 
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/fontawesome-free/css/all.min.css">
 
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
 
     <!-- DataTables -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 
     <!-- Select2 -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
 
     <!-- JQVMap -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/jqvmap/jqvmap.min.css">
 
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/dist/css/adminlte.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/dist/css/custom.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/dist/css/adminlte.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/dist/css/custom.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/assets/css/styles.css">
 
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
 
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/daterangepicker/daterangepicker.css">
 
     <!-- summernote -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/summernote/summernote-bs4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/summernote/summernote-bs4.min.css">
 
     <!-- SweetAlert2 -->
-    <link rel="stylesheet" href="<?php echo base_url ?>stylesheet/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url ?>utils/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     
     <style type="text/css">/* Chart.js */
         @keyframes chartjs-render-animation{
@@ -79,23 +79,23 @@
     </style>
 
      <!-- jQuery -->
-    <script src="<?php echo base_url ?>stylesheet/plugins/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url ?>utils/plugins/jquery/jquery.min.js"></script>
 
     <!-- jQuery UI 1.11.4 -->
-    <script src="<?php echo base_url ?>stylesheet/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="<?php echo base_url ?>utils/plugins/jquery-ui/jquery-ui.min.js"></script>
 
     <!-- SweetAlert2 -->
-    <script src="<?php echo base_url ?>stylesheet/plugins/sweetalert2/sweetalert2.min.js"></script>
+    <script src="<?php echo base_url ?>utils/plugins/sweetalert2/sweetalert2.min.js"></script>
 
     <!-- Toastr -->
-    <script src="<?php echo base_url ?>stylesheet/plugins/toastr/toastr.min.js"></script>
+    <script src="<?php echo base_url ?>utils/plugins/toastr/toastr.min.js"></script>
 
     <script>
         var _base_url_ = '<?php echo base_url ?>';
     </script>
 
-    <script src="<?php echo base_url ?>stylesheet/dist/js/script.js"></script>
-    <script src="<?php echo base_url ?>stylesheet/assets/js/scripts.js"></script>
+    <script src="<?php echo base_url ?>utils/dist/js/script.js"></script>
+    <script src="<?php echo base_url ?>utils/assets/js/scripts.js"></script>
 
     <style>
         #main-header{
