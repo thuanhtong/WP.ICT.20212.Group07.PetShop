50af60403988c2384633f6287b5c8b11025183e0
giangntt <nguyenthugiang106@gmail.com>
2022-07-23 07:41:23 +0700

    ignore intialize.php

--- PetShop/initialize.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('base_url')) define('base_url','http://localhost:8080/WP.ICT.20212.Group07.PetShop/PetShop/');
-if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost"); 
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"1123");
-if(!defined('DB_NAME')) define('DB_NAME',"pet_shop_db");
-?>
-
