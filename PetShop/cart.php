f8ecdd9c572a2e9a461c75975a98cf7d70f44ef2
giangntt <nguyenthugiang106@gmail.com>
2022-07-23 17:03:15 +0700

    add view cart page front end

---
+++ PetShop/cart.php
@@ -0,0 +1,60 @@
+
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-end mb-2">
+                <button class="btn btn-outline-dark btn-flat btn-sm" type="button" id="empty_cart">Empty Cart</button>
+            </div>
+        </div>
+        <div class="card rounded-0">
+            <div class="card-body">
+                <h3><b>Cart List</b></h3>
+                <hr class="border-dark">
+                <?php 
+                    $qry = $conn->query("SELECT c.*,p.product_name,i.size,i.price,p.id as pid from `cart` c inner join `inventory` i on i.id=c.inventory_id inner join products p on p.id = i.product_id where c.client_id = ".$_settings->userdata('id'));
+                    while($row= $qry->fetch_assoc()):
+                        $upload_path = base_app.'/uploads/product_'.$row['pid'];
+                        $img = "";
+                        if(is_dir($upload_path)){
+                            $fileO = scandir($upload_path);
+                            if(isset($fileO[2]))
+                                $img = "uploads/product_".$row['pid']."/".$fileO[2];
+                        }
+                ?>
+                    <div class="d-flex w-100 justify-content-between  mb-2 py-2 border-bottom cart-item">
+                        <div class="d-flex align-items-center col-8">
+                            <span class="mr-2"><a href="javascript:void(0)" class="btn btn-sm btn-outline-danger rem_item" data-id="<?php echo $row['id'] ?>"><i class="fa fa-trash"></i></a></span>
+                            <img src="<?php echo validate_image($img) ?>" loading="lazy" class="cart-prod-img mr-2 mr-sm-2 border" alt="">
+                            <div>
+                                <p class="mb-1 mb-sm-1"><?php echo $row['product_name'] ?></p>
+                                <p class="mb-1 mb-sm-1"><small><b>Size:</b> <?php echo $row['size'] ?></small></p>
+                                <p class="mb-1 mb-sm-1"><small><b>Price:</b> <span class="price"><?php echo number_format($row['price']) ?></span></small></p>
+                                <div>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <button class="btn btn-sm btn-outline-secondary min-qty" type="button" id="button-addon1"><i class="fa fa-minus"></i></button>
+                                    </div>
+                                    <input type="number" class="form-control form-control-sm qty text-center cart-qty" placeholder="" aria-label="Example text with button addon" value="<?php echo $row['quantity'] ?>" aria-describedby="button-addon1" data-id="<?php echo $row['id'] ?>" readonly>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-sm btn-outline-secondary plus-qty" type="button" id="button-addon1"><i class="fa fa-plus"></i></button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col text-right align-items-center d-flex justify-content-end">
+                            <h4><b class="total-amount"><?php echo number_format($row['price'] * $row['quantity']) ?></b></h4>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+                <div class="d-flex w-100 justify-content-between mb-2 py-2 border-bottom">
+                    <div class="col-8 d-flex justify-content-end"><h4>Grand Total:</h4></div>
+                    <div class="col d-flex justify-content-end"><h4 id="grand-total">-</h4></div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex w-100 justify-content-end">
+            <a href="./?p=checkout" class="btn btn-sm btn-flat btn-dark">Checkout</a>
+        </div>
+    </div>
+</section>
