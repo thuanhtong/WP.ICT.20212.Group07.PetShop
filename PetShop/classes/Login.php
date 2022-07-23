6bdf0421072acfe451330849df13a3d24742d6b5
giangntt <nguyenthugiang106@gmail.com>
2022-07-23 08:11:32 +0700

    login for user

---
+++ PetShop/classes/Login.php
@@ -0,0 +1,47 @@
+<?php
+require_once '../config.php';
+class Login extends DBConnection {
+	private $settings;
+	public function __construct(){
+		global $_settings;
+		$this->settings = $_settings;
+
+		parent::__construct();
+		ini_set('display_error', 1);
+	}
+	public function __destruct(){
+		parent::__destruct();
+	}
+	public function index(){
+		echo "<h1>Access Denied</h1> <a href='".base_url."'>Go Back.</a>";
+	}
+	function login_user(){
+		extract($_POST);
+		$qry = $this->conn->query("SELECT * from clients where email = '$email' and password = md5('$password') ");
+		if($qry->num_rows > 0){
+			foreach($qry->fetch_array() as $k => $v){
+				$this->settings->set_userdata($k,$v);
+			}
+			$this->settings->set_userdata('login_type',1);
+		$resp['status'] = 'success';
+		}else{
+		$resp['status'] = 'incorrect';
+		}
+		if($this->conn->error){
+			$resp['status'] = 'failed';
+			$resp['_error'] = $this->conn->error;
+		}
+		return json_encode($resp);
+	}
+}
+$action = !isset($_GET['f']) ? 'none' : strtolower($_GET['f']);
+$auth = new Login();
+switch ($action) {
+	case 'login_user':
+		echo $auth->login_user();
+		break;
+	default:
+		echo $auth->index();
+		break;
+}
+
